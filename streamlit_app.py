838b5bfeb89080261781a2746bea3e44daf92e24
Trang Ng <87792252+BuzzzyNYC@users.noreply.github.com>
2022-11-12 16:20:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# streamlit.text(fruityvice_response.json()) #writes the data to the screen
+
+# take the json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it as a table
+streamlit.dataframe(fruityvice_normalized)
 
 
 # After pulling the data into a pandas dataframe called my_fruit_list, we will ask the streamlit library to display it on the page by typing:
